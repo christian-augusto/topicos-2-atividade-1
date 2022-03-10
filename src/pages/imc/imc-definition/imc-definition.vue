55d877fb7a3ec42e9cbec7f8cbfa3f6adf3aa31a
Christian Augusto Martins dos Santos Barros <christian.barros@enjoei.com.br>
2022-03-10 14:00:45 -0300

    feat: finishing imc definition

---
+++ src/pages/imc/imc-definition/imc-definition.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="imc-definition">
+    <div class="imc-definition__wrapper flex">
+      <h1 class="main__title" v-html="translations.translation('pageTitle')"></h1>
+    </div>
+    <div class="imc-definition__wrapper flex">
+      <h4 class="main__sub-title" v-html="translations.translation('definitionTitle')"></h4>
+      <p v-html="translations.translation('definitionText')"></p>
+    </div>
+    <div class="imc-definition__wrapper flex">
+      <h4 class="main__sub-title" v-html="translations.translation('howMeasureTitle')"></h4>
+      <p v-html="translations.translation('howMeasureText')"></p>
+    </div>
+  </section>
+</template>
+
+<script>
+import Translations from "@/translations";
+import translationsData from "./translations.json";
+import "./imc-definition.scss";
+
+const translations = new Translations(translationsData);
+
+export default {
+  data() {
+    return {
+      translations,
+    };
+  },
+};
+</script>
--- src/pages/imc/imc.vue
+++ src/pages/imc/imc.vue
@@ -1,11 +1,13 @@
 <template>
   <main role="main" class="imc">
+    <vImcDefinition />
     <vImcTable />
     <vImcCalc />
   </main>
 </template>
 
 <script>
+import vImcDefinition from "./imc-definition/imc-definition.vue";
 import vImcTable from "./imc-table/imc-table.vue";
 import vImcCalc from "./imc-calc/imc-calc.vue";
 
@@ -13,6 +15,7 @@ import "./imc.scss";
 
 export default {
   components: {
+    vImcDefinition,
     vImcTable,
     vImcCalc,
   },
